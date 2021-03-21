8da422b5d108fbfcee623c25ca6f7852358867d6
Adrian Milsap <adrian@hirobius.com>
2021-03-20 20:29:02 -0700

    img url remaining

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -4,9 +4,9 @@ class HornedBeasts extends React.Component {
   render() {
     return (
       <div className="beastContainer">
-        <h2 className="title">Beast Name</h2>
-        <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"></img>
-        <p className="description">Lorem Ipsum Placeholder Description</p>
+        <h2 className="title">{this.props.title}</h2>
+        <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" alt="Beast" title="Beast"></img>
+        <p className="description">{this.props.description}</p>
       </div>
     );
   }
--- src/Main.js
+++ src/Main.js
@@ -5,9 +5,9 @@ class Main extends React.Component {
   render() {
     return (
       <div className="beastContainer">
-        <HornedBeasts title="Beast Name 1" description="Lorem Ipsum Placeholder Description!!!"/>
-        <HornedBeasts title="Beast Name 2" description="Lorem Ipsum Placeholder Description!!!"/>
-        <HornedBeasts title="Beast Name 3" description="Lorem Ipsum Placeholder Description!!!"/>
+        <HornedBeasts title="Beast Name 1" description="Lorem Ipsum Placeholder Description 1"/>
+        <HornedBeasts title="Beast Name 2" description="Lorem Ipsum Placeholder Description 2"/>
+        <HornedBeasts title="Beast Name 3" description="Lorem Ipsum Placeholder Description 3"/>
       </div>
     );
   }
