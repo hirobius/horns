7810bd427be6cc57ff8066b38ad6a39b1a24244a
Adrian Milsap <adrian@hirobius.com>
2021-03-23 22:41:37 -0700

    progress capture

--- src/App.js
+++ src/App.js
@@ -3,13 +3,36 @@ import React from 'react';
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
+import Button from 'react-bootstrap/Button';
 
 class App extends React.Component {
+  constructor(props) {
+    // this is always the first line of our constructor within a component
+    super(props);
+    this.state = {
+      numberOfExclamationPoints: 1,
+    };
+  }
+
+  buttonClicked = () => {
+    console.log('buttonclicked!');
+    // sadly, we cannot just write:
+    // this.state.numberOfExclamationPoints++
+    // instead, we write:
+    this.setState({
+      numberOfExclamationPoints: this.state.numberOfExclamationPoints + 1
+    });
+  }
+
   render() {
+    // let exclamationPoints = () => numberOfExclamationPoints + 1;
     return (
       <div>
         <Header />
         <Main />
+        <h2>Set up {this.state.numberOfExclamationPoints}</h2>
+        <button onClick={this.buttonClicked}>Test!</button>
+        <Button>hmmm...</Button>
         <Footer />
       </div>
     );
--- src/Main.js
+++ src/Main.js
@@ -5,7 +5,7 @@ class Main extends React.Component {
   render() {
     return (
       <div className="beastContainer">
-        <HornedBeasts title="Beast Name 1" description="Lorem Ipsum Placeholder Description 1"/>
+        <HornedBeasts title="Beast Name 1" description="Lorem Ipsum Placeholder Description 1" url="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"/>
         <HornedBeasts title="Beast Name 2" description="Lorem Ipsum Placeholder Description 2"/>
         <HornedBeasts title="Beast Name 3" description="Lorem Ipsum Placeholder Description 3"/>
       </div>
